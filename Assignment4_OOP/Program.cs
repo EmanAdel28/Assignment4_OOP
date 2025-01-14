89b1e075d47b4dbf6acf946d74b38fe4cd6d42ee
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-14 07:44:40 +0200

    Part1_Q1

--- Assignment4_OOP/Program.cs
+++ Assignment4_OOP/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part1
+            #region Q1
+            //Question 1:
+            //What is the primary purpose of an interface in C#?
+            //a) To provide a way to implement multiple inheritance
+            //b) To define a blueprint for a class
+            //c) To declare abstract methods and properties
+            //d) To create instances of objects
+
+            //Solution => b) To define a blueprint for a class
+            #endregion
+            #endregion
+
         }
     }
 }
